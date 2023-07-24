b7ae7a89e7b319d1fa80f0e68da49787d8fbc87f
Chanuth10 <it20074418@my.sliit.lk>
2023-07-24 11:46:34 +0530

    implement user delete and update for the controller file

--- controller/UserController.js
+++ controller/UserController.js
@@ -38,7 +38,6 @@ exports.createUser = catchAsyncErrors(async (req, res, next) => {
   }
 });
 
-// Login User
 exports.loginUser = catchAsyncErrors(async (req, res, next) => {
   const { email, password } = req.body;
 
@@ -64,4 +63,159 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
   sendToken(user, 201, res);
 });
 
+exports.logoutUser = catchAsyncErrors(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
 
+  res.status(200).json({
+    success: true,
+    message: "Log out success",
+  });
+});
+
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorHandler("User not found with this email", 404));
+  }
+
+const resetToken = user.getResetToken();
+
+  await user.save({
+    validateBeforeSave: false,
+  });
+
+  const resetPasswordUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/password/reset/${resetToken}`;
+
+  const message = `Your password reset token is :- \n\n ${resetPasswordUrl}`;
+
+  try {
+    await sendMail({
+      email: user.email,
+      subject: `Ecommerce Password Recovery`,
+      message,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: `Email sent to ${user.email} succesfully`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordTime = undefined;
+
+    await user.save({
+      validateBeforeSave: false,
+    });
+
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
+
+exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
+
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.token)
+    .digest("hex");
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordTime: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(
+      new ErrorHandler("Reset password url is invalid or has been expired", 400)
+    );
+  }
+
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(
+      new ErrorHandler("Password is not matched with the new password", 400)
+    );
+  }
+
+  user.password = req.body.password;
+
+  user.resetPasswordToken = undefined;
+  user.resetPasswordTime = undefined;
+
+  await user.save();
+
+  sendToken(user, 200, res);
+});
+
+exports.userDetails = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+   
+    const user = await User.findById(req.user.id).select("+password");
+
+    const isPasswordMatched = await user.comparePassword(req.body.oldPassword);
+
+    if (!isPasswordMatched) {
+      return next(
+        new ErrorHandler("Old Password is incorrect", 400)
+      );
+    };
+
+    if(req.body.newPassword  !== req.body.confirmPassword){
+        return next(
+            new ErrorHandler("Password not matched with each other", 400)
+          );
+    }
+
+    user.password = req.body.newPassword;
+
+    await user.save();
+
+    sendToken(user,200,res);
+});
+
+exports.updateProfile = catchAsyncErrors(async(req,res,next) =>{
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email,
+    };
+
+   if (req.body.avatar !== "") {
+    const user = await User.findById(req.user.id);
+
+    const imageId = user.avatar.public_id;
+
+    await cloudinary.v2.uploader.destroy(imageId);
+
+    const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+      folder: "avatars",
+      width: 150,
+      crop: "scale",
+    });
+    newUserData.avatar = {
+      public_id: myCloud.public_id,
+      url: myCloud.secure_url,
+    };
+  }
+
+  const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+    new: true,
+    runValidator: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+  });
+});
