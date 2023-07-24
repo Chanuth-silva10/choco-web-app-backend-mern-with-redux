782172e3651005954359ca040a832e4edb2e6890
Chanuth10 <it20074418@my.sliit.lk>
2023-07-24 11:37:06 +0530

    add user route into the app.js file

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-/** @format */
-
 const express = require("express");
 const app = express();
 const ErrorHandler = require("./middleware/error");
 const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(fileUpload({ useTempFiles: true }));
 
+const user = require("./routes/UserRoute");
 
 app.use("/api/v2", user);
 
-
-
-
+app.use(ErrorHandler);
 
 module.exports = app;
