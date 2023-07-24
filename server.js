100c6f5a407c9d182a608fa2959052b9c3364014
Chanuth10 <it20074418@my.sliit.lk>
2023-07-24 11:12:24 +0530

    create server.js and install relavent dependencies

---
+++ server.js
@@ -0,0 +1,23 @@
+const app = require("./app");
+const connectDatabase = require("./db/Database.js");
+require("dotenv").config();
+
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server for Handling uncaught Exception`);
+});
+
+connectDatabase();
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
+
+// Unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Shutting down server for ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled promise rejection`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
