f68cdb1e390e2c1736e79a230dc516bf2cdc67f6
Chanuth10 <it20074418@my.sliit.lk>
2023-07-24 11:37:57 +0530

    add user controller for the login and register

---
+++ controller/UserController.js
@@ -0,0 +1,67 @@
+const User = require("../models/UserModel");
+const ErrorHandler = require("../utils/ErrorHandler.js");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const sendToken = require("../utils/jwtToken.js");
+const sendMail = require("../utils/sendMail.js");
+const crypto = require("crypto");
+const cloudinary = require("cloudinary");
+
+exports.createUser = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const { name, email, password, avatar } = req.body;
+
+    let user = await User.findOne({ email });
+    if (user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
+    }
+
+    const myCloud = await cloudinary.v2.uploader.upload(avatar, {
+      folder: "avatars",
+    });
+
+    user = await User.create({
+      name,
+      email,
+      password,
+      avatar: { public_id: myCloud.public_id, url: myCloud.secure_url },
+    });
+    
+    sendToken(user, 201, res);
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+// Login User
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter the email & password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(
+      new ErrorHandler("User is not find with this email & password", 401)
+    );
+  }
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(
+      new ErrorHandler("User is not find with this email & password", 401)
+    );
+  }
+
+  sendToken(user, 201, res);
+});
+
+
